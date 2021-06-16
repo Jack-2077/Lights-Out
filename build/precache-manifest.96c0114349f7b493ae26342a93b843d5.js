602e456f2905cf15dd200e7f967df92a09cbcb19
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-06-16 16:08:54 -0700

    Deploy to GitHub Pages

---
+++ build/precache-manifest.96c0114349f7b493ae26342a93b843d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dee2b276e051cfc15370",
+    "url": "/Lights-Out/static/css/main.e4596e84.chunk.css"
+  },
+  {
+    "revision": "dee2b276e051cfc15370",
+    "url": "/Lights-Out/static/js/main.dee2b276.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/Lights-Out/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "bcedacc98465ac2cb9bb",
+    "url": "/Lights-Out/static/js/runtime~main.bcedacc9.js"
+  },
+  {
+    "revision": "d6b700a9a7ec3d1895c3b9715862be91",
+    "url": "/Lights-Out/index.html"
+  }
+];
